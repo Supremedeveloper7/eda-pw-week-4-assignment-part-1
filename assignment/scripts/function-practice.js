2f5cfc1b72a229c0cda21a8672aad52355c199d4
supreme <nurhassan696@gmail.com>
2021-12-05 20:03:49 -0600

    submitting part 1

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -1,3 +1,4 @@
+
 console.log('***** Function Practice *****')
 
 // Add the required code to complete the functions below
@@ -6,78 +7,112 @@ console.log('***** Function Practice *****')
 
 // 1. Function to return 'Hello World!'
 function hello() {
-  return 'Hello World!';
+return 'Hello World!';
+console.log('Hello World', hello());
 }
 // Call the function to test
 console.log('Test - should say "Hello World!"', hello());
-
+console.log(hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  const greeting = 'Hello and welcome , ' + name;
+  return greeting;
 }
 // Remember to call the function to test
-
+console.log(helloName('Ibrahim') );
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber, secondNumber ) {
+  const result = firstNumber + secondNumber;
+  return result;
   // return firstNumber + secondNumber;
 }
+console.log(addNumbers(15, 30));
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-
+function multiplyThree(num1, num2, num3 ){
+     const multiply = num1 * num2 * num3;
+     return multiply;
 }
-
+console.log(multiplyThree(3, 6, 16) );
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
-  }
-    return;
+    return true;
+  }else
+    return false;
 }
+console.log(isPositive(2) );
+
+
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
 console.log( 'isPositive - should say true', isPositive(3) );
 console.log( 'isPositive - should say false', isPositive(0) );
 console.log( 'isPositive - should say false', isPositive(-3) );
+console.log(isPositive(3));
+console.log(isPositive(0));
+console.log(isPositive(-3));
+
+
 
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
-function getLast( array ) {
-
+function getLast( array ) { 
+  console.log(array[2]);
+  return undefined;
 }
-
+console.log(getLast[0,1,2]);
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find( value, array ){
-  
-}
+  console.log( 'in find:', value, array);
+  let arr = [0,1,2]
+  for(i=0; i<arr.length; i++)
+  if( array === value){
+    return true;
+  } else {
+    return
+  }
 
-// ----------------------
+
+
+}
+  console.log( 'running find', find());
+
+
+
 // Stretch Goals
 // ----------------------
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
+  if( string = 'apple'){
+    return true;
+  }
+  }
 
-}
+
+
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
 function sumAll( ) {
+  console.log( 'in sumAll:',);
   let sum = 0
   // TODO: loop to add items
+  
   return sum;
 }
-
+console.log(sumAll(0));
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
@@ -86,4 +121,4 @@ function sumAll( ) {
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
-//     here in a comment, write the function, and test it!
+//     here in a comment, write the function, and test it.
